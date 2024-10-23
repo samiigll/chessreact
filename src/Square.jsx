93b8e66e4a8cb5cb799967ac74060fe2f1a2653f
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-10-23 15:34:58 +0300

    Refactor Square and SqureBoard components

---
+++ src/Square.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Square = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Square;
---
+++ src/SqureBoard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SqureBoard = () => {
+  return <div>SqureBoard</div>;
+};
+
+export default SqureBoard;
